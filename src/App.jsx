62536c5c44d7d889d839aead35f9a500b942129e
Fawad Ahmad <fawad7998@gmail.com>
2024-10-23 16:52:02 +0500

    Feat:ReactPractice on some small module

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-import Translation from "./translation"
+import {
+  BrowserRouter,
+  Link,
+  Route,
+  Routes,
+} from "react-router-dom";
+import ImageGallery from "./LazyImage/lazyImage"
+import Test from "./ModelComponent/Test";
+import Home from "./Home";
 
 
 function App() {
 
   return (
     <>
-      <Translation />
+
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/test" element={<Test />} />
+          <Route path="/image" element={<ImageGallery />} />
+        </Routes>
+      </BrowserRouter>
     </>
 
   )
